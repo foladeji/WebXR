3587fc8ad00244eb1f83d9f726fa5ece35aab507
foladeji <folade2@illinois.edu>
2021-10-14 22:46:06 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -223,7 +223,7 @@ class App {
         this.workingQuaternion = new THREE.Quaternion();
         this.raycaster = new THREE.Raycaster();
 
-        window.addEventListener('onSelectStart', this.changePosition.bind(this));
+        window.addEventListener('tap', this.changePosition.bind(this));
         // window.addEventListener('touch', this.changePosition.bind(this));
     
     }
